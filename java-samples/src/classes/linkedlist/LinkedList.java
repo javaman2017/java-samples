7a2063b6757c251946ba96c053f57cb11d8e9575
javaman2017 <tommooney2017@outlook.com>
2017-07-21 22:27:23 +0100

    Added a main method to create a linked list of vehicle objects

--- java-samples/src/classes/linkedlist/LinkedList.java
+++ java-samples/src/classes/linkedlist/LinkedList.java
@@ -1,6 +1,33 @@
 package classes.linkedlist;
 
+import classes.vehicles.Vehicle;
+
 public class LinkedList {
 	public Object value;
 	public LinkedList next;
+	public static void main(String[] args){
+		Vehicle vehicle1 = new Vehicle();
+		vehicle1.currentSpeed = 10;
+		vehicle1.currentDirection = 1;
+		vehicle1.ownerName = "Tom";
+		
+		Vehicle vehicle2 = new Vehicle();
+		vehicle2.currentSpeed = 20;
+		vehicle2.currentDirection = -1;
+		vehicle2.ownerName = "Dick";
+		
+		Vehicle vehicle3 = new Vehicle();
+		vehicle3.currentSpeed = 30;
+		vehicle3.currentDirection = 0;
+		vehicle3.ownerName = "Harry";
+		
+		LinkedList vehicles = new LinkedList();
+		
+		vehicles.value = vehicle1;
+		vehicles.next = new LinkedList();
+		
+		vehicles.next.value = vehicle2;
+		vehicles.next.next = new LinkedList();
+		vehicles.next.next.value = vehicle3;
+	}
 }
