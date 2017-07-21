f86f0f82959fc53163f1605f0e3ef1d369cd9187
javaman2017 <tommooney2017@outlook.com>
2017-07-21 19:44:36 +0100

    Version 1 of Vehicle.java

---
+++ java-samples/src/classes/vehicles/Vehicle.java
@@ -0,0 +1,9 @@
+package classes.vehicles;
+
+public class Vehicle {
+	public static long nextID = 0;
+	public long   id;
+	public double currentSpeed;
+	public double currentDirection; // currentDirection is in degrees from north
+	public String ownerName;
+}
