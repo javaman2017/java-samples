5fd26fefef4a55c931b987602b81f3fd8f57f3aa
javaman2017 <tommooney2017@outlook.com>
2017-07-21 19:49:27 +0100

    Added main method to create a few Vehicle objects and print their values

--- java-samples/src/classes/vehicles/Vehicle.java
+++ java-samples/src/classes/vehicles/Vehicle.java
@@ -6,4 +6,35 @@ public class Vehicle {
 	public double currentSpeed;
 	public double currentDirection; // currentDirection is in degrees from north
 	public String ownerName;
+	
+	public static void main(String[] args){
+		Vehicle vehicle1 = new Vehicle();
+		vehicle1.id = Vehicle.nextID;
+		vehicle1.currentSpeed = 10;
+		vehicle1.currentDirection = 1;
+		vehicle1.ownerName = "Tom";
+		Vehicle.nextID++;
+		
+		Vehicle vehicle2 = new Vehicle();
+		vehicle2.id = Vehicle.nextID;
+		vehicle2.currentSpeed = 20;
+		vehicle2.currentDirection = -1;
+		vehicle2.ownerName = "Dick";
+		Vehicle.nextID++;
+		
+		Vehicle vehicle3 = new Vehicle();
+		vehicle3.id = Vehicle.nextID;
+		vehicle3.currentSpeed = 30;
+		vehicle3.currentDirection = 0;
+		vehicle3.ownerName = "Harry";
+		Vehicle.nextID++;
+		
+		Vehicle[] vehicles = new Vehicle[] {vehicle1, vehicle2, vehicle3};
+		
+		for(int i = 0; i < vehicles.length; i++){
+			System.out.println("id=" + vehicles[i].id + ": " + "currentSpeed=" + vehicles[i].currentSpeed
+					+ ", currentDirection=" + vehicles[i].currentDirection + ", ownerName="
+					+ vehicles[i].ownerName);					
+		}
+	}
 }
