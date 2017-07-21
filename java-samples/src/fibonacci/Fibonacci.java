5080dd7093144c46b2ade15d524f9531d9b6c77a
javaman2017 <tommooney2017@outlook.com>
2017-07-21 19:56:30 +0100

    Version1 of Fibonacci.java

---
+++ java-samples/src/fibonacci/Fibonacci.java
@@ -0,0 +1,21 @@
+package fibonacci;
+
+public class Fibonacci {
+	static final int MAX = 50;	// named constant
+
+	/** Print out the Fibonacci sequence for values < MAX_INDEX */
+	public static void main(String[] args) {
+		int lo = 1;
+		int hi = 1;
+		
+		System.out.println(lo);
+		while(hi < MAX){
+			System.out.println(hi);
+			hi = lo + hi;	// new hi
+			lo = hi - lo; 	/* new lo is (sum - old lo)
+							   that is, the old hi */
+		}
+
+	}
+
+}
