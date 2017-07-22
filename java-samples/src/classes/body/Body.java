905f0a8fcbe0d6d53dff032b5d80b74e643ab4d2
javaman2017 <tommooney2017@outlook.com>
2017-07-22 22:31:41 +0100

    Made nextID private and we now initalize idNum inside the constructor and increment nextID internally

--- java-samples/src/classes/body/Body.java
+++ java-samples/src/classes/body/Body.java
@@ -3,7 +3,11 @@ package classes.body;
 public class Body {
 	public long idNum;
 	public String name = "<unnamed>";
-	public Body orbits;
+	public Body orbits = null;
 	
-	public static long nextID = 0;
+	private static long nextID = 0;
+	
+	Body(){
+		idNum = nextID++;
+	}
 }
