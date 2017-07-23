94e4c127150774d3723977b2486a0d3fe59be84a
javaman2017 <tommooney2017@outlook.com>
2017-07-23 22:14:57 +0100

    Added a basic copy constructor

--- java-samples/src/classes/body/Body.java
+++ java-samples/src/classes/body/Body.java
@@ -21,11 +21,19 @@ public class Body {
 		this(bodyName,null);
 	}
 	
+	Body(Body other){
+		idNum = other.idNum;
+		name = other.name;
+		orbits = other.orbits;
+	}
+	
 	public static void main(String[] args){
 		Body sun = new Body("Sun");
 		Body earth = new Body("Earth",sun); 
+		Body earth2 = new Body(earth);
 	
 		System.out.println("The " + earth.name + " orbits the " + sun.name);
+		System.out.println("Earth2 orbits " + earth2.orbits.name);
 		
 	}
 }
