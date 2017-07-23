8db18710b035327423718dc85a4f89e23540c721
javaman2017 <tommooney2017@outlook.com>
2017-07-23 22:27:22 +0100

    Added constructors for LinkedList.java and update the main method to reflect the changes

--- java-samples/src/classes/linkedlist/LinkedList.java
+++ java-samples/src/classes/linkedlist/LinkedList.java
@@ -5,29 +5,31 @@ import classes.vehicles.Vehicle;
 public class LinkedList {
 	public Object value;
 	public LinkedList next;
+	
+	LinkedList(Object value, LinkedList next){
+		this.value = value;
+		this.next = next;
+	}
+	
+	LinkedList(Object value){
+		this(value,null);
+	}
+	
 	public static void main(String[] args){
-		Vehicle vehicle1 = new Vehicle();
+		Vehicle vehicle1 = new Vehicle("Tom");
 		vehicle1.currentSpeed = 10;
 		vehicle1.currentDirection = 1;
-		vehicle1.ownerName = "Tom";
 		
-		Vehicle vehicle2 = new Vehicle();
+		Vehicle vehicle2 = new Vehicle("Dick");
 		vehicle2.currentSpeed = 20;
 		vehicle2.currentDirection = -1;
-		vehicle2.ownerName = "Dick";
 		
-		Vehicle vehicle3 = new Vehicle();
+		Vehicle vehicle3 = new Vehicle("Harry");
 		vehicle3.currentSpeed = 30;
 		vehicle3.currentDirection = 0;
-		vehicle3.ownerName = "Harry";
 		
-		LinkedList vehicles = new LinkedList();
-		
-		vehicles.value = vehicle1;
-		vehicles.next = new LinkedList();
-		
-		vehicles.next.value = vehicle2;
-		vehicles.next.next = new LinkedList();
-		vehicles.next.next.value = vehicle3;
+		LinkedList tail = new LinkedList(vehicle3);
+		LinkedList node2 = new LinkedList(vehicle2,tail);
+		LinkedList head = new LinkedList(vehicle1,node2);
 	}
 }
