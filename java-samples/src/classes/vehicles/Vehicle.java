5654db29f743eaddebd56ae21cf50194c83f7696
javaman2017 <tommooney2017@outlook.com>
2017-07-23 22:21:22 +0100

    Added no-arg and constructor taking ownerName. Also modified the main method with the corresponding changes

--- java-samples/src/classes/vehicles/Vehicle.java
+++ java-samples/src/classes/vehicles/Vehicle.java
@@ -7,26 +7,30 @@ public class Vehicle {
 	public double currentDirection; // currentDirection is in degrees from north
 	public String ownerName;
 	
+	Vehicle(){		
+	}
+	
+	public Vehicle(String ownerName){
+		this.ownerName = ownerName;
+	}
+	
 	public static void main(String[] args){
-		Vehicle vehicle1 = new Vehicle();
+		Vehicle vehicle1 = new Vehicle("Tom");
 		vehicle1.id = Vehicle.nextID;
 		vehicle1.currentSpeed = 10;
 		vehicle1.currentDirection = 1;
-		vehicle1.ownerName = "Tom";
 		Vehicle.nextID++;
 		
-		Vehicle vehicle2 = new Vehicle();
+		Vehicle vehicle2 = new Vehicle("Dick");
 		vehicle2.id = Vehicle.nextID;
 		vehicle2.currentSpeed = 20;
 		vehicle2.currentDirection = -1;
-		vehicle2.ownerName = "Dick";
 		Vehicle.nextID++;
 		
-		Vehicle vehicle3 = new Vehicle();
+		Vehicle vehicle3 = new Vehicle("Harry");
 		vehicle3.id = Vehicle.nextID;
 		vehicle3.currentSpeed = 30;
 		vehicle3.currentDirection = 0;
-		vehicle3.ownerName = "Harry";
 		Vehicle.nextID++;
 		
 		Vehicle[] vehicles = new Vehicle[] {vehicle1, vehicle2, vehicle3};
