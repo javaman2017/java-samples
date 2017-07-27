cc281a179235e60c01de7c64c6fdba26584cdcc3
javaman2017 <tommooney2017@outlook.com>
2017-07-27 17:40:33 +0100

    Added vararg setter method for private field driverNames. Moved id generation/assignment code into initialization block. Implemented a toString method to return an objects state and finally made other public fields private that don't need to be public anymore

--- java-samples/src/classes/vehicles/Vehicle.java
+++ java-samples/src/classes/vehicles/Vehicle.java
@@ -1,51 +1,54 @@
 package classes.vehicles;
 
 public class Vehicle {
-	public static long nextID = 0;
+	private static long nextID = 0;
 	
 	public static long currentHighestId(){
 		return (nextID - 1);
 	}
 	
-	public long   id;
+	private long   id;
 	public double currentSpeed;
 	public double currentDirection; // currentDirection is in degrees from north
-	public String ownerName;
+	private String ownerName;
+	private String[] driverNames = null;
 	
-	Vehicle(){		
+	{
+		id = nextID;
+		nextID++;
 	}
 	
 	public Vehicle(String ownerName){
 		this.ownerName = ownerName;
 	}
 	
+	public void setDriverNames(String... driverNames){
+		this.driverNames = driverNames;
+	}
+	
+	@Override
+	public String toString() {
+		String desc = "Vehicle [id=" + id + ", currentSpeed=" + currentSpeed + ", currentDirection=" + currentDirection
+				+ ", ownerName=" + ownerName;
+		if (driverNames != null){
+			desc += ", drivers=[";
+			desc += ownerName + ", ";
+			int count = driverNames.length;
+			for(int i = 0; i < count; i++){
+				desc += driverNames[i];
+				if (i < count - 1)
+					desc += ", ";
+			}
+		}
+		desc += "]";
+		return desc;
+	}
+
 	public static void main(String[] args){
 		Vehicle vehicle1 = new Vehicle("Tom");
-		vehicle1.id = Vehicle.nextID;
 		vehicle1.currentSpeed = 10;
 		vehicle1.currentDirection = 1;
-		Vehicle.nextID++;
-		
-		Vehicle vehicle2 = new Vehicle("Dick");
-		vehicle2.id = Vehicle.nextID;
-		vehicle2.currentSpeed = 20;
-		vehicle2.currentDirection = -1;
-		Vehicle.nextID++;
-		
-		Vehicle vehicle3 = new Vehicle("Harry");
-		vehicle3.id = Vehicle.nextID;
-		vehicle3.currentSpeed = 30;
-		vehicle3.currentDirection = 0;
-		Vehicle.nextID++;
-		
-		System.out.println("Highest id used so far: " + Vehicle.currentHighestId());
-		
-		Vehicle[] vehicles = new Vehicle[] {vehicle1, vehicle2, vehicle3};
-		
-		for(int i = 0; i < vehicles.length; i++){
-			System.out.println("id=" + vehicles[i].id + ": " + "currentSpeed=" + vehicles[i].currentSpeed
-					+ ", currentDirection=" + vehicles[i].currentDirection + ", ownerName="
-					+ vehicles[i].ownerName);					
-		}
+		vehicle1.setDriverNames("Dick","Harry");
+		System.out.println(vehicle1);
 	}
 }
