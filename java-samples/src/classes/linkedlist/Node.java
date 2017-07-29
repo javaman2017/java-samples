8094d161b9245df393b22e526cf270736ab1607a
javaman2017 <tommooney2017@outlook.com>
2017-07-29 16:59:49 +0100

    Version1 of Node.java

---
+++ java-samples/src/classes/linkedlist/Node.java
@@ -0,0 +1,32 @@
+package classes.linkedlist;
+
+public class Node {
+	private Object value;
+	private Node next;
+	
+	public Node(Object value, Node next){
+		this.value = value;
+		this.next = next;
+	}
+	
+	public Node(Object value){
+		this(value,null);
+	}
+	
+	public Object getValue() {
+		return value;
+	}
+
+	@Override
+	public String toString() {
+		return "Node [value=" + value + ", next=" + next + "]";
+	}
+
+	public void setValue(Object value) {
+		this.value = value;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+}
