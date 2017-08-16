70b628846d268284a03add80636fe174e467d47e
javaman2017 <tommooney2017@outlook.com>
2017-08-16 16:35:30 +0100

    Initial versions of classes illustrating a sorting harness

---
+++ java-samples/src/classes/sorting/SortMetrics.java
@@ -0,0 +1,28 @@
+package classes.sorting;
+
+public class SortMetrics implements Cloneable{
+    public long probeCnt,       // simple data probes
+                compareCnt,     // comparing two elements
+                swapCnt;        // swapping two elements
+
+    public void init() {
+        probeCnt = swapCnt = compareCnt = 0;
+    }
+    
+    public String toString(){
+        return probeCnt + " probes " +
+               compareCnt + " compares " + 
+               swapCnt + " swaps ";
+    }
+    
+    /** This class supports clone **/
+    public SortMetrics clone(){
+        try{
+            // default mechanism works
+            return (SortMetrics)super.clone();
+        } catch(CloneNotSupportedException e){
+            // can't happen this and Object both clone
+            throw new InternalError(e.toString());
+        }
+    }
+}
