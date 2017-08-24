754790571865c3a2f6a7274738a261554dac8ce9
javaman2017 <tommooney2017@outlook.com>
2017-08-24 13:48:43 +0100

    Added constructor. Added an inner class History to store the bank accounts last 10 actions. Added method to return history as an Iteratable of Actions

--- java-samples/src/classes/nestedtypes/BankAccount.java
+++ java-samples/src/classes/nestedtypes/BankAccount.java
@@ -1,14 +1,25 @@
 package classes.nestedtypes;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class BankAccount {
-    private long number;    // account number
-    private long balance;   // current balance (in cents)
-    private Action lastAct; // last action performed
+private long number;              // account number
+    private long balance;         // current balance (in cents)
+    private History actHistory;   // account history       
     
     public static class Permissions {
         public boolean canDeposit,
                        canWithdraw,
                        canClose;
+    }
+    
+    public BankAccount(long number, long balance) {
+        super();
+        this.number = number;
+        this.balance = balance;
+        actHistory = new History();
     }
     
     public class Action {
@@ -24,25 +35,63 @@ public class BankAccount {
         }
     }
     
+    public class History implements Iterable<Action>{
+        List<Action> actHistory = new ArrayList<>();   // action history
+        void addAct(Action newAct){
+            if(actHistory.size() > 10){
+                actHistory.clear(); // we keep only 10 previous records
+             } 
+             actHistory.add(newAct);
+        }
+    
+        @Override
+        public Iterator<Action> iterator() {
+            return actHistory.iterator();
+        }
+    }
+    
+    public History history(){
+        return this.actHistory;
+    }
+    
     public void deposit(long amount){
         balance += amount;
-        lastAct = new Action("deposit",amount);
+        actHistory.addAct(new Action("deposit",amount));
     }
     
     public void withdraw(long amount){
         balance -= amount;
-        lastAct = new Action("withdraw",amount);
+        actHistory.addAct(new Action("withdraw",amount));
     }
     
     public void transfer(BankAccount other, long amount){
         other.withdraw(amount);
         deposit(amount);
-        lastAct = this.new Action("transer",amount);
-        other.lastAct = other.new Action("transfer",amount);
+        actHistory.addAct(this.new Action("transer",amount));
+        other.actHistory.addAct(other.new Action("transfer",amount));
     }
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        BankAccount bankAccount1 = new BankAccount(123456, 100);
+        
+        //We do some deposits and withdrawals on bankAccount1
+        bankAccount1.deposit(100);
+        bankAccount1.deposit(50);
+        bankAccount1.withdraw(10);
+        
+        
+        BankAccount bankAccount2 = new BankAccount(789,200);
+        
+        //Transfer some funds from bankAccount1 to bankAccount2
+        bankAccount1.transfer(bankAccount2, 50);
+        
+        Iterator<Action> histIter1 = bankAccount1.history().iterator();
+        while(histIter1.hasNext()){
+            System.out.println(histIter1.next());
+        }
+        
+        Iterator<Action> histIter2 = bankAccount2.history().iterator();
+        while(histIter2.hasNext()){
+            System.out.println(histIter2.next());
+        }
     }
-
 }
