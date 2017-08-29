311af3b2b5e0b35853b845b9e22397d8d730bf93
javaman2017 <tommooney2017@outlook.com>
2017-08-29 22:39:44 +0100

    Illustrates local inner class

---
+++ java-samples/src/classes/nestedtypes/CollectionUtils.java
@@ -0,0 +1,38 @@
+package classes.nestedtypes;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class CollectionUtils {
+    
+    public static Iterator<Object> walkThrough(final Object[] objs){
+        class Iter implements Iterator<Object> {
+            private int pos = 0;
+            public boolean hasNext(){
+                return (pos < objs.length);
+            }
+            
+            public Object next() throws NoSuchElementException {
+                if (pos >= objs.length)
+                    throw new NoSuchElementException();
+                return objs[pos++];
+            }
+            
+            public void remove(){
+                throw new UnsupportedOperationException();
+            }
+        }
+        
+        return new Iter();
+    }
+
+    public static void main(String[] args) {
+        String[] names = {"Tom", "Dick", "Harry"};
+        Iterator<Object> iter = CollectionUtils.walkThrough(names);
+      
+        while(iter.hasNext())
+            System.out.println("name: " + iter.next());
+
+    }
+
+}
