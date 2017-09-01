e68b30f6af808ebbfaea3e410927db6150397096
javaman2017 <tommooney2017@outlook.com>
2017-09-01 21:45:00 +0100

    Updated to use a one arg constructor that takes a name for each enum instance

--- java-samples/src/classes/enums/Suit.java
+++ java-samples/src/classes/enums/Suit.java
@@ -1,5 +1,14 @@
 package classes.enums;
 
 public enum Suit {
-    CLUBS,  DIAMONDS, HEARTS, SPADES
+    CLUBS("CLUBS"),  
+    DIAMONDS("DIAMONDS"), 
+    HEARTS("HEARTS"), 
+    SPADES("SPADES"),
+    ;
+    
+    String name;
+    Suit(String name){ this.name = name; }
+    
+    public String toString(){ return this.name; }
 }
