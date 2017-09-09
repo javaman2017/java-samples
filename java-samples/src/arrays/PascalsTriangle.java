87ae46de70c2b655c874a110010011a25497c8b5
javaman2017 <tommooney2017@outlook.com>
2017-09-10 00:36:08 +0100

    Refactored to be more readable

--- java-samples/src/arrays/PascalsTriangle.java
+++ java-samples/src/arrays/PascalsTriangle.java
@@ -9,22 +9,21 @@ public class PascalsTriangle {
             System.exit(0);
         }
         
-        int depth = Integer.parseInt(args[0]);
-        int[][] pascalsTriangle = new int[depth][];
+        int triangleDepth = Integer.parseInt(args[0]);
+        int[][] pascalsTriangle = new int[triangleDepth][];
         pascalsTriangle[0] = new int[]{1};
-        int rowCount = pascalsTriangle.length;
-        for(int row = 1; row < rowCount; row++){
-            //Create a new row to hold the next line of pascal's numbers
+        for(int row = 1; row < triangleDepth; row++){
+            //Create a array to hold the next row of Pascal's triangle
             int prevRowLength = pascalsTriangle[row - 1].length;
-            int newRowLength = prevRowLength + 1;
-            int[] newRow = new int[newRowLength];
-            
-            //The start and end elements are always 1
+            int[] newRow = new int[prevRowLength + 1];
+                       
+            //The start/end numbers are 1 in all rows
+            int newRowLength = newRow.length;
             newRow[0] = 1;
             newRow[newRowLength - 1] = 1;
             int newCol = 1;
             
-            //Populate the middle elements using pascal's recurrence relation
+            //Compute and populate the missing Pascal numbers for the current row
             for(int col = 1; col < newRowLength - 1; col++){
                 int[] prevRow = pascalsTriangle[row - 1];
                 for (int preCol = 0; preCol < prevRowLength - 1; preCol++){
@@ -34,11 +33,11 @@ public class PascalsTriangle {
                 }
             }
             
-            //Assign the new row of pascal's numbers to the triangle
+            //Assign the new row to the triangle
             pascalsTriangle[row] = newRow;
         }
         
-        //Print out pascal's triangle
+        //Print out Pascal's triangle
         for(int[] row : pascalsTriangle)
             System.out.println(Arrays.toString(row));
 
