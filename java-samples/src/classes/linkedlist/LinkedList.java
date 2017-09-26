3928169609d0dd3ff6ef4b806fd4f204301dba3d
javaman2017 <tommooney2017@outlook.com>
2017-09-26 20:03:22 +0100

    Generified LinkedList. Moved Node into it as a nested inner class and deleted the original Node.java

--- java-samples/src/classes/linkedlist/LinkedList.java
+++ java-samples/src/classes/linkedlist/LinkedList.java
@@ -1,45 +1,60 @@
 package classes.linkedlist;
 
-import java.util.Arrays;
-
 import classes.vehicles.Vehicle;
 
-public class LinkedList {
+public class LinkedList<E> {
 	private static final int DEFAULT_LENGTH= 10;
-	private Node[] nodes = null;
-	private int size = 0;
+	private Node<E>[] nodes;
+	private int size;
 	
 	@Override
 	public String toString() {
-		return "LinkedList [nodes=" + Arrays.toString(nodes) + "]";
+		String dump = "";
+		for(Node<E> node : nodes)
+		    dump += node + "\n";
+		return dump;
 	}
 
 	public LinkedList(){
 		nodes = new Node[DEFAULT_LENGTH];
 	}
 	
-	public LinkedList(Node... nodes){
-		this.nodes = nodes;
+    public LinkedList(E... elements){
+        int length = elements.length;
+		this.nodes = new Node[length];
+		for(int i = 0; i < length; i++)
+		    this.add(elements[i]);
 	}
 	
-	public boolean add(Node node){
+	public boolean add(E newElement){
 		boolean result = false;
+        Node<E> newNode = new Node();
+        newNode.value = newElement;
 		if (size > 0){
-			Node currentHead = nodes[size];
-			Node oldHead = new Node(currentHead,node);
-			nodes[size] = oldHead;
-			nodes[size++] = node;
+			Node<E> headNode = nodes[size - 1];
+			headNode.next = newNode;
+			nodes[size++] = newNode;
 			result = true;
 		} else {
-			nodes[size++] = node;
+			nodes[size++] = newNode;
 			result = true;
 		}
 		
-		return true;
+		return result;
 	}
 	
 	public int size(){
 		return size;
+	}
+	
+	private class Node<E> {
+	    E value;
+	    Node<E> next;
+        
+        @Override
+        public String toString() {
+            return "Node [value=" + value + ",\n next=" + next + "]";
+        }
 	}
 
 	public static void main(String[] args){
@@ -55,18 +70,14 @@ public class LinkedList {
 		vehicle3.setCurrentSpeed(30);
 		vehicle3.setCurrentDirection(0);
 		
-		Node tail = new Node(vehicle3);
-		Node node2 = new Node(vehicle2,tail);
-		Node head = new Node(vehicle1,node2);
-
-		LinkedList linkedList = new LinkedList();
-		linkedList.add(head);
-		linkedList.add(node2);
-		linkedList.add(tail);
+		LinkedList<Vehicle> linkedList = new LinkedList<>(vehicle1, vehicle2, vehicle3);
 		
 		System.out.println(linkedList);
 		
 		System.out.println("Number of elements in linkedList: " + linkedList.size());
+		
+		LinkedList<Integer> integerLinkedList = new LinkedList<>(1,2,3);
+		System.out.println(integerLinkedList);
 		
 	}
 }
