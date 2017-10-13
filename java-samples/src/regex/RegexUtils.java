86b26f6139365146b91ce6aecd1a0dbb000099af
mr-x <mr-x@mr-x-computer>
2017-10-13 14:40:20 +0100

    Added replaceWithNextLargestNumber

--- java-samples/src/regex/RegexUtils.java
+++ java-samples/src/regex/RegexUtils.java
@@ -11,16 +11,37 @@ public class RegexUtils {
         String result = matcher.replaceAll("moon");
         return result;
     }
+    
+    public static String replaceWithNextLargestNumber(String input) {
+        Pattern pat = Pattern.compile("[-+]?[0-9]+");
+        Matcher matcher = pat.matcher(input);
+        StringBuffer result = new StringBuffer();
+        boolean found;
+        while((found = matcher.find())) {
+            String numStr = matcher.group();
+            int num = Integer.parseInt(numStr);
+            String plusOne = Integer.toString(num + 1);
+            matcher.appendReplacement(result, plusOne);
+        }
+        matcher.appendTail(result);
+        
+        return result.toString();
+    }
 
 
     public static void main(String[] args) {
-        String input = "The sun shines so bright. The sun is high in the sky";
+        String input1 = "The sun shines so bright. The sun is high in the sky";
         
-        System.out.println("Before a replace operation: " + input);
+        System.out.println("Before a replace operation: " + input1);
+                
+        System.out.println("After a replace operation: " + replaceAll(input1,"sun"));
         
-        String result = replaceAll(input,"sun");
+        String input2 = "The numbers are 19 and 131";
         
-        System.out.println("After a replace operation: " + result);
+        System.out.println("Before a replace operation: " + input2);
+                
+        System.out.println("After a replace operation: " + replaceWithNextLargestNumber(input2));
+        
 
     }
 }
