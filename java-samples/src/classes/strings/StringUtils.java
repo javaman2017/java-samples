5eb1fbefc26ca08295b952bb96f5a7c23970a80a
mr-x <mr-x@mr-x-computer>
2017-10-13 16:19:13 +0100

    Added hashedQuote method

--- java-samples/src/classes/strings/StringUtils.java
+++ java-samples/src/classes/strings/StringUtils.java
@@ -26,10 +26,24 @@ public class StringUtils {
         return dslist.toArray(new String[dslist.size()]);   
     }
     
+    public static String hashedQuote(String quote) {
+        return '#' + quote + '#';
+    }
+    
+    
+    
     public static void main(String[] args) {       
        String s = "Il a dit #Bonjour!# #Bonjour!#";
        String[] tokens = StringUtils.delimitedString("Il a dit #Bonjour!# #Bonjour!# #Bonjour!# #J'e ma pelle#",'#','#');
        for(String token: tokens)
            System.out.println(token);
+       System.out.println();
+       
+       String quote = "To be or not to be";
+       long start = System.nanoTime();
+       for(int i = 0; i < 10000; i++)
+           hashedQuote(quote);
+       long elapsedtime = System.nanoTime() - start;
+       System.out.println("Elapsed time to hash quote 10000 times: " + elapsedtime);
     }
 }
