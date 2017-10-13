7559d8f6235d61b907ed5948104d4c8f6bb67340
mr-x <mr-x@mr-x-computer>
2017-10-13 14:21:01 +0100

    Renamed to RegexUtils and created static method replaceAll

--- java-samples/src/regex/RegexUtils.java
+++ java-samples/src/regex/RegexUtils.java
@@ -3,19 +3,24 @@ package regex;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class FindAndReplace {
+public class RegexUtils {
+    
+    public static String replaceAll(String input,String regex) {
+        Pattern pat = Pattern.compile(regex);
+        Matcher matcher = pat.matcher(input);
+        String result = matcher.replaceAll("moon");
+        return result;
+    }
+
 
     public static void main(String[] args) {
         String input = "The sun shines so bright. The sun is high in the sky";
         
         System.out.println("Before a replace operation: " + input);
         
-        Pattern pat = Pattern.compile("sun");
-        Matcher matcher = pat.matcher(input);
-        String result = matcher.replaceAll("moon");
+        String result = replaceAll(input,"sun");
         
         System.out.println("After a replace operation: " + result);
 
     }
-
 }
