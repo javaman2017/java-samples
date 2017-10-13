cec5432abf42236a4c33a0fa8ca90bc8c267d157
mr-x <mr-x@mr-x-computer>
2017-10-13 22:25:14 +0100

    Method to comma-tize number strings

--- java-samples/src/classes/strings/StringUtils.java
+++ java-samples/src/classes/strings/StringUtils.java
@@ -31,6 +31,64 @@ public class StringUtils {
                     .append(quote).append('#').toString();
     }
     
+    public static void replace(StringBuilder str, char oldChar, char newChar) {
+        for(int i = 0; i < str.length(); i++)
+            if(str.charAt(i) == oldChar)
+                str.setCharAt(i, newChar);
+    }
+    
+    public static String sqrtInt(int i) {
+        StringBuilder buf = new StringBuilder();
+        
+        buf.append("sqrt(").append(i).append(')');
+        buf.append(" = ").append(Math.sqrt(i));
+        return buf.toString();
+    }
+    
+    public static StringBuilder addDate(StringBuilder buf) {
+        String now = new java.util.Date().toString();
+        buf.insert(0,now).insert(now.length(), ": ");
+        return buf;
+    }
+    
+    public static StringBuilder remove(StringBuilder buf, int pos, int cnt) {
+        if (pos < 0 || cnt < 0 || pos + cnt > buf.length())
+            throw new IndexOutOfBoundsException();
+        
+        int leftover = buf.length() - (pos + cnt);
+        if (leftover == 0) {    //a simple truncation
+            buf.setLength(pos);
+            return buf;
+        }
+        
+        char[] chrs = new char[leftover];
+        buf.getChars(pos + cnt, buf.length(), chrs, 0);
+        buf.setLength(pos);
+        buf.append(chrs);
+        return buf;
+    }
+    
+    public static String convertToCommaSequence(String numStr) {
+        if (numStr == null || numStr.length() == 0)
+            throw new IllegalArgumentException();
+        
+        int len = numStr.length();
+        if (len <= 3)
+            return numStr;
+        
+        String DELIMATOR = ",";
+        StringBuilder buf = new StringBuilder(numStr);
+      
+        buf.insert(len - 3, DELIMATOR);
+        int leftover = buf.length() - 4;
+        for(int i = buf.length() - 7; leftover > 3 && i > 0; i-=3) {
+            buf.insert(i, DELIMATOR);
+            leftover = buf.length() - i;
+        }
+        
+        return buf.toString();
+    }
+    
     public static void main(String[] args) {       
        String s = "Il a dit #Bonjour!# #Bonjour!#";
        String[] tokens = StringUtils.delimitedString("Il a dit #Bonjour!# #Bonjour!# #Bonjour!# #J'e ma pelle#",'#','#');
@@ -54,6 +112,25 @@ public class StringUtils {
        double totalRunningTime = 0;
        for(long rt: runningTimes)
            totalRunningTime += rt;
-       System.out.println("Average running time of 10,000 calls to hashQuote for 10,000 iterations: " + (totalRunningTime/runningTimes.size()));
+       System.out.println("Average running time of 10,000 calls to hashQuote for 10,000 iterations: " + 
+                          (totalRunningTime/runningTimes.size()));
+       
+       StringBuilder str = new StringBuilder("abc");
+       StringUtils.replace(str,'c','b');
+       System.out.println(str);
+       
+       System.out.println(sqrtInt(2));
+       
+       StringBuilder logMsg = new StringBuilder("Initialised application");
+       System.out.println(addDate(logMsg));
+       
+       StringBuilder buf = new StringBuilder("Tom, Dick and Harry");
+       System.out.println(buf);
+       System.out.println("Harry has to go: " + remove(buf,14,5));
+       
+       String[] numStrs = {"1","12","123","1234","12345","123456","1234567","12345678","123456789","987654321989"};
+       for(String numStr : numStrs)
+           System.out.println("Old numStr: " + numStr + ", new numStr: " + convertToCommaSequence(numStr));
+      
     }
 }
