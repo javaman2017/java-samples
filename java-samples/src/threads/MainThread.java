4004eae7d049449abd1011a505a007cdbb579cdf
mr-x <mr-x@mr-x-computer>
2017-10-14 22:00:40 +0100

    Gets the name of the thread that executes main

---
+++ java-samples/src/threads/MainThread.java
@@ -0,0 +1,7 @@
+package threads;
+
+public class MainThread {
+    public static void main(String[] args) {
+       System.out.println("Thread that executes main: " + Thread.currentThread().getName());
+    }
+}
