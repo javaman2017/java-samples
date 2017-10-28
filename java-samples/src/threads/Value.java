a230cbdec5e7c670a4a9a3e69af7042c146e0d66
javaman2017 <thomascmooney@hotmail.co.uk>
2017-10-28 20:46:44 +0100

    showing similar with static members and method synchronization

--- java-samples/src/threads/Value.java
+++ java-samples/src/threads/Value.java
@@ -3,65 +3,62 @@ package threads;
 import java.util.concurrent.CountDownLatch;
 
 public class Value {
-    private int value;
+    private static int value;
 
-    public int getValue() {
+    public static int getValue() {
         return value;
     }
+    public static void setValue(int value) { Value.value = value;}
 
-    public void add(int increment){
+    public static synchronized void add(int increment){
         value += increment;
     }
 
 
     public static void main(String[] args) throws InterruptedException {
-        CountDownLatch latch = new CountDownLatch(3);
+        for(int run = 0; run < 10; run++) {
+            CountDownLatch latch = new CountDownLatch(3);
 
-        int increment = 1;
-        Value value = new Value();
-        Thread threadA = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                    synchronized (value) {
+            int increment = 1;
+            Value value = new Value();
+            Thread threadA = new Thread(new Runnable() {
+                @Override
+                public void run() {
                         for (int i = 0; i < 1000; i++)
                             value.add(increment);
 
                         latch.countDown();
-                    }
-
-            }
-        },"threadA");
-
-        Thread threadB = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                synchronized (value) {
-                    for (int i = 0; i < 1000; i++)
-                        value.add(increment);
-
-                    latch.countDown();
                 }
-            }
-        },"threadB");
+            }, "threadA");
 
-        Thread threadC = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                synchronized (value) {
-                    for (int i = 0; i < 1000; i++)
-                        value.add(increment);
+            Thread threadB = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                        for (int i = 0; i < 1000; i++)
+                            value.add(increment);
 
-                    latch.countDown();
+                        latch.countDown();
                 }
-            }
-        },"threadC");
+            }, "threadB");
 
-        threadA.start();
-        threadB.start();
-        threadC.start();
+            Thread threadC = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                        for (int i = 0; i < 1000; i++)
+                            value.add(increment);
 
-        latch.await();
-        System.out.println("Final value = " + value.getValue());
+                        latch.countDown();
+                }
+            }, "threadC");
+
+            threadA.start();
+            threadB.start();
+            threadC.start();
+
+            latch.await();
+            System.out.println("Run " + run + " with final value = " + value.getValue());
+            value.setValue(0);
+        }
 
     }
 }
