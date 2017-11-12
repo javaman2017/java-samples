6a1d3bd96ff4fdd85a54a2cd91e0607e4847f1f5
javaman2017 <abcDef123?>
2017-11-12 12:14:55 +0000

    Restriction of Revision annotations to type declarations

--- java-samples/src/annotations/Revision.java
+++ java-samples/src/annotations/Revision.java
@@ -1,5 +1,9 @@
 package annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
 public @interface Revision {
     int major() default 1;
     int minor() default 0;
