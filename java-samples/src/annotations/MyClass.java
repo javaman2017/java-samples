38f71db558b05b4d88bfaa799ab57374632d3543
javaman2017 <abcDef123?>
2017-11-12 11:52:27 +0000

    New major revision number is 4

--- java-samples/src/annotations/MyClass.java
+++ java-samples/src/annotations/MyClass.java
@@ -4,7 +4,7 @@ package annotations;
            createdBy = "Mr T",
            lastModified = "665",
            lastModifiedBy = "Mr T",
-           revision = @Revision(major = 3))
+           revision = @Revision(major = 4))
 @BugsFixed({"123","456"})
 public class MyClass {
     private int i;
